6f816c1a6ac3214e526d95630771ffc49a6ea6bf
xinfei1 <xinfei1@staff.weibo.com>
2020-06-17 11:51:19 +0800

    test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { collision } from './utils/collision';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test('collision interface', () => {
+  const a = {
+    gridx: 0,
+    gridy: 0,
+    width: 2,
+    height: 1
+  };
+  const b = {
+    gridx: 1,
+    gridy: 0,
+    width: 1,
+    height: 1
+  };
+  expect(collision(a,b)).toEqual(true);
 });
